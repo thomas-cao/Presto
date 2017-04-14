9efd11a888709b24a6d8fdb68444215461477428
emppu－cao <759362837@qq.com>
2017-04-14 16:46:16 +0800

    init
    初始化项目

---
+++ Presto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Presto
+//
+//  Created by emppu－cao on 2017/4/14.
+//  Copyright © 2017年 emppu－cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
